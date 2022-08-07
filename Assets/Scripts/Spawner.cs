4204e7919c4bead8eadcd761d1583408a67cf4d6
Ihor <dasada162@gmail.com>
2022-08-07 20:28:06 +0400

    First commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : PoolObjects
+{
+    [SerializeField] private GameObject[] _enemyTemplate;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _spawnTime;
+
+    private float _elapsedTime;
+
+    private void Start()
+    {
+        Initialize(_enemyTemplate);
+    }
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _spawnTime)
+        {
+            if (TryGetObject(out GameObject enemy))
+            {
+                _elapsedTime = 0; 
+
+                int _numberOfSpawnPoint = Random.Range(0, _spawnPoints.Length);
+                
+                SetEnemy(enemy, _spawnPoints[_numberOfSpawnPoint].position);
+            }
+        }
+    }
+    private void SetEnemy(GameObject enemy, Vector3 spawnPoint)
+    {
+        enemy.SetActive(true);
+        enemy.transform.position = spawnPoint;
+    }
+}
